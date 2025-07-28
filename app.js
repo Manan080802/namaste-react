09c89073c00b707fb7d538da512e0910b1038d85
Manan080802 <manvaghasiya882@gmail.com>
2025-07-28 20:47:43 +0530

    Add the jsx

--- app.js
+++ app.js
@@ -29,13 +29,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Create heading elements with unique keys
-const heading1 = React.createElement("h1", { key: "h1" }, "Nested React!");
-const heading2 = React.createElement("h2", { key: "h2" }, "Nested React!");
+// const heading1 = React.createElement("h1", { key: "h1" }, "Nested React!");
+// const heading2 = React.createElement("h2", { key: "h2" }, "Nested React!");
 
-// Create child and parent divs
-const child = React.createElement("div", { id: "child" }, [heading1, heading2]);
-const parent = React.createElement("div", { id: "parent" }, child);
+// // Create child and parent divs
+// const child = React.createElement("div", { id: "child" }, [heading1, heading2]);
+// const parent = React.createElement("div", { id: "parent" }, child);
 
-// Create root and render to #root
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// // Create root and render to #root
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+
+// const heading  = React.createElement("h1",{key:"h1"},"Manan Vaghasiya");
+
+const heading = <h1>Manan Vaghasiya using jsx</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading);
