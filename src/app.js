ce903194011f979205e97ef3c2453d08e1071e01
Manan080802 <manvaghasiya882@gmail.com>
2025-08-10 15:03:09 +0530

    Add the child routing and link implement

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import RectDOM from "react-dom/client";
 import Header from "./components/Header";
 import Footer from "./components/footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Body from "./components/Body";
 import About from "./components/About";
 import Error from "./components/Error";
@@ -12,7 +12,8 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
+      {/* <Body /> */}
       <Footer />
     </div>
   );
@@ -22,16 +23,30 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
   },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
+  // {
+  //   path: "/about",
+  //   element: <About />,
+  // },
+  // {
+  //   path: "/contact",
+  //   element: <Contact />,
+  // },
 ]);
 
 const root = RectDOM.createRoot(document.getElementById("root"));
