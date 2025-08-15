124345298369f19884b3ebf658e0f7764b5c8f89
Manan080802 <manvaghasiya882@gmail.com>
2025-08-15 21:06:12 +0530

    Add the group the test case

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,25 @@
 const Contact = () => {
   return (
-    <div className="res-about">
-      <h2>Contact</h2>
-      <h3>Manan Vaghasiya</h3>
+    <div className="container  mx-auto px-4">
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+      <p>dfff</p>
+      <span>sdfscls</span>
+
+      <form className="p-4 m-4">
+        <input
+          type="text"
+          placeholder="Enter name"
+          className="border-2  mx-2 p-2"
+        />
+        <input
+          type="text"
+          placeholder="Enter message"
+          className="border-2 mx-2 p-2"
+        />
+        <button className="mx-2 p-2 bg-orange-300 text-white w-20 h-10">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
